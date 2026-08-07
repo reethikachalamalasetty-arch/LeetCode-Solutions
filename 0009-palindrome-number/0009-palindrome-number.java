d9066905a0b26f84e0f846a890383666032a2112
chalamalasetty reethika <reethikachalamalasetty@gmail.com>
2026-08-08 00:57:20 +0530

    Time: 5 ms (81.26%), Space: 45.2 MB (99.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,5 @@
 class Solution {
-    public boolean isPalindrome(int x) 
-    {
-
-       
+    public boolean isPalindrome(int x) {
         if (x < 0) {
             return false;
         }
@@ -10,7 +7,7 @@ class Solution {
         int original = x;
         int reverse = 0;
 
-        while (x != 0) {
+        while (x > 0) {
             int digit = x % 10;
             reverse = reverse * 10 + digit;
             x = x / 10;

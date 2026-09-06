37a5e093caea1b3ff4613c886cdb14b2ac7b8910
Reethika Chalamalasetty <reethikachalamalasetty@gmail.com>
2026-09-06 08:55:03 +0530

    Time: 37 ms (47.42%), Space: 47.2 MB (68.15%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Empty string matches empty pattern
+        dp[0][0] = true;
+
+        // Empty string can match a pattern containing only '*'
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                // Same character or '?'
+                if (pc == '?' || pc == sc) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // '*' can match empty or one/more characters
+                else if (pc == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
